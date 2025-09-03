d598e9049a4cf988e21eeebc30e99510cc1e7770
LAPTOP-ORHKVD26\al_fa <al_fabien@hotmail.fr>
2025-09-03 18:28:03 +0200

    doc

--- front/src/app/products/data-access/cart.service.ts
+++ front/src/app/products/data-access/cart.service.ts
@@ -5,23 +5,35 @@ import { Product } from "./product.model";
     providedIn: 'root',
 })
 export class CartService {
-    private _productsInCart = signal<{ cartItemId: number, product: Product }[]>([]);
-    private currentId = 0;
+    private _productsInCart = signal<{ cartItemId: number, product: Product }[]>([]); //Signaux des articles présents dans le panier
+    private currentId = 0; //Sert d'identification dans le cas ou le même article serait ajouté plusieurs fois au panier
     public readonly productsInCart = this._productsInCart.asReadonly();
 
+    /**
+     * Ajoute un article au panier, lui attribue un identifiant unique
+     */
     addToCart(product: Product) {
         const item = { cartItemId: this.currentId++, product };
         this._productsInCart.update((items) => [...items, item]);
     }
 
+    /**
+     * Suppression d'un article du panier par son identifiant
+     */
     deleteFromCart(cartItemId: number) {
         this._productsInCart.update((items) => items.filter(i => i.cartItemId !== cartItemId));
     }
 
+    /**
+     * Cout total de l'ensemble des articles présents dans le panier
+     */
     getTotalCartCost() : number {
         return this._productsInCart().reduce((total, item) => total + item.product.price, 0);
     }
 
+    /**
+     * Nombre d'article dans le panier
+     */
     getCartAmountOfProducts() : number {
         return this._productsInCart().length;
     }
